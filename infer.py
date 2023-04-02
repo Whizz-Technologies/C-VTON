911cbafbe30abba1efbc55ebfb6bc4b6ca3ba41f
Gaurav614 <gauravbarua614@gmail.com>
2023-04-02 12:17:20 +0530

    Added Infer scripy and MPV dataset needs all the segm mask

--- infer.py
+++ infer.py
@@ -26,4 +26,38 @@ model = models.OASIS_model(opt)
 model = models.put_on_multi_gpus(opt, model)
 model.eval()
 
-dataset = dataset_cl(opt, phase=opt.phase)
+
+evaluate(model, dataset, opt)
+
+model = models.OASIS_model(opt)
+model = models.put_on_multi_gpus(opt, model)
+model.eval()
+
+dataset = dataset_cl(opt, phase=opt.phase, test_pairs='/data/mpv/test_pairs.txt')
+
+test_dataloader = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False)
+
+os.makedirs(os.path.join("results", opt.name, opt.phase + "_images"), exist_ok=True)
+
+for i, data_i in enumerate(test_dataloader):
+    print(i, "/", len(test_dataloader), end="\r")
+    image, label = models.preprocess_input(opt, data_i)
+    # label["cloth_seg"] = model.module.edit_cloth_seg(image["C_t"], label["body_seg"], label["cloth_seg"])
+    agnostic = data_i["agnostic"] if opt.bpgm_id.find("old") >= 0 else None
+    
+    if opt.no_seg:
+        image["I_m"] = image["I"]
+    
+    pred = model(image, label, "generate", None, agnostic=agnostic).detach().cpu().squeeze().permute(1, 2, 0).numpy()
+    pred = (pred + 1) / 2
+    
+    pred = (pred * 255).astype(np.uint8)
+    pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
+    # pred = cv2.resize(pred, (data_i['original_size'][1], data_i['original_size'][0]), interpolation=cv2.INTER_LINEAR)
+    pred = cv2.resize(pred, opt.img_size[::-1], interpolation=cv2.INTER_AREA)
+    
+    if opt.dataset == "mpv":
+        filename = data_i['name'][0].split("/")[-1].replace(".jpg", ".png")
+    elif opt.dataset == "viton":
+        filename = data_i['name'][0].split("/")[-1]
+    cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename), pred)
